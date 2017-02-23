1a7c0c01cd85eda91881f9f66c6ce729f6bafb6a
Samuel Younkin <samuel.younkin@gmail.com>
2017-02-23 12:42:08 -0600

    Created documentation for package
    Use help('scenic') to see the documentation.

---
+++ R/packageInfo.R
@@ -0,0 +1,15 @@
+#' Scenario Generation Tool (scenic)
+#'
+#' Description: An implementation of scenario generation based on
+#' travel survey data. It takes into account variable names and type
+#' of scenarios users are interested in. See the ICT website for more
+#' information, documentation and examples.
+#' 
+#' @name scenic-package
+#' @docType package
+#' @author Ali Abbas \email{aa797@@cam.ac.uk}
+#' @references \url{http://www.cedar.iph.cam.ac.uk/research/modelling/ithim/}, \url{https://ithim.ghi.wisc.edu/}
+#' @aliases scenic
+#' @examples
+#'
+NULL
