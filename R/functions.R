b7a8e61ed4106f559f1ef553d172f288d8c25bf7
usr110 <m.aliabbas@gmail.com>
2017-02-24 15:21:56 +0000

    Update return description for bike_choice

--- R/functions.R
+++ R/functions.R
@@ -50,7 +50,7 @@ trip_speed <- function(age, sex, bike_type)  {
 #'
 #' @param dist Trip Distance
 #' @param tripsebike Probability of converting a trip into an ebike trip based on trip distance
-#' @return Trip Speed based on age, sex and bike_type
+#' @return Returns a boolean: 1 for ebike and 0 for pedal bike
 #' @export
 
 
@@ -58,11 +58,11 @@ bike_choice <-function(dist, tripsebike) {
   #calculates prob of switch to cycling depending on: [age-sex-trip distance]
   
   #intervals for distance binning
-  distIntervals <-c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,9.5,12.5,15.5,20.5,30.5,40.5,10000)
+  distIntervals <- c(0.5,1.5,2.5,3.5,4.5,5.5,6.5,9.5,12.5,15.5,20.5,30.5,40.5,10000)
   
   
   #get interval to use as nrow
-  nrow <- findInterval(dist,distIntervals)+1  # starts @ 0, add 1
+  nrow <- findInterval(dist, distIntervals)+1  # starts @ 0, add 1
   # Comment out hard coded trips ebike probabilities
   # probstrip <- c(0.550,0.680,0.751,0.815,0.889,0.889,0.905,0.929,0.947,0.919,0.919,1.000,1.000,0.000)
   
